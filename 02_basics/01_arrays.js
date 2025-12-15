18b63f57bbbf68c93967474f6d8b78707a402830
SamarAlamKhan <alamkhansamar@gmail.com>
2025-12-15 07:48:00 +0000

    container config done

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+// Array
+
+const myArray = [1, 2, 3, 4, 5];
+const myHeroes = ["Ironman", "Thor", "Captain America", "Hulk", "Spiderman", "Wolverine"];
+const myArray2 = new Array("a", "b", "c", "d", "e");
+
+console.log(myArray);
+console.log(myArray2[3]);
+console.log(typeof myArray);
+
+// Array Methods
+myArray.push(6);
+myArray.push(7);
+console.log(myArray);
+myArray.pop();
+console.log(myArray);
